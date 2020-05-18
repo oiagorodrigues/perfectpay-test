262590091572567e53811769458a6daa62cffefb
Iago <yagomello16@gmail.com>
2020-05-18 16:59:57 -0300

    feat(components): add Navbar layout component

---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,30 @@
+<script>
+export default {
+  name: 'TheNavbar',
+}
+</script>
+
+<template>
+  <b-navbar
+    class="the-navbar bg-white"
+    variant="faded"
+    type="light"
+    tag="header"
+  >
+    <b-container>
+      <b-navbar-brand class="the-navbar--brand">PerfectPay.fun</b-navbar-brand>
+    </b-container>
+  </b-navbar>
+</template>
+
+<style lang="scss" scoped>
+.the-navbar {
+  padding: $navbar-height 0;
+  margin-bottom: 25px;
+
+  &--brand {
+    font-size: 30px;
+    font-weight: bold;
+  }
+}
+</style>
