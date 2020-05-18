c387ed000ab47b357355b6e348a4849766fb0316
Iago <yagomello16@gmail.com>
2020-05-18 17:06:05 -0300

    feat(views): implement MoneyBar partials in Home View
    Added MoneyBar local partial to Home View

---
+++ src/views/Home/partials/MoneyBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <section class="money-bar position-sticky text-white text-center">
+    <h2 v-if="totalMoney" class="money-bar--title mb-0">
+      {{ totalMoney | currency }}
+    </h2>
+    <h2 v-else class="money-bar--title mb-0">Acabou a grana 😭</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'MoneyBar',
+  props: {
+    totalMoney: {
+      type: [String, Number],
+      required: true,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.money-bar {
+  z-index: 9;
+  top: -1px;
+  padding: 20px;
+  margin: 10px 0px;
+  background-color: #2ecc71;
+
+  &--title {
+    font-size: 24px;
+
+    @include media-breakpoint-up(sm) {
+      font-size: 36px;
+    }
+  }
+}
+</style>
