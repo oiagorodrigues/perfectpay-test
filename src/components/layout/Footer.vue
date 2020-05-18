6c8411b5a35709d6a627910635c83f9f71ba587c
Iago <yagomello16@gmail.com>
2020-05-18 16:59:45 -0300

    feat(components): add Footer layout component

---
+++ src/components/layout/Footer.vue
@@ -0,0 +1,42 @@
+<script>
+export default {
+  name: 'TheFooter',
+}
+</script>
+
+<template>
+  <footer class="the-footer bg-white text-center">
+    <b-container>
+      <div class="the-footer--author">
+        <span>Made with ❤️ by </span>
+        <b-link
+          class="the-footer--link"
+          href="https://www.linkedin.com/in/iago-rodrigues/"
+          target="_blank"
+          >Iago Rodrigues</b-link
+        >
+      </div>
+      <div class="the-footer--referrer">
+        <span>Website clone of </span>
+        <b-link
+          class="the-footer--link"
+          href="https://neal.fun/spend/"
+          target="_blank"
+          >Neil.fun</b-link
+        >
+      </div>
+    </b-container>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.the-footer {
+  padding: $footer-height 20px ($footer-height + 1em);
+  margin-top: 15px;
+
+  &--link {
+    color: $secondary;
+    font-weight: bold;
+  }
+}
+</style>
