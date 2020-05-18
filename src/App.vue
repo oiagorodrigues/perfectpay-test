bdaa83b5054edb72cdce32d661d0595c7cc80b0f
Iago <yagomello16@gmail.com>
2020-05-18 17:00:31 -0300

    feat(components): implement App structure

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,23 @@
+<script>
+export default {
+  name: 'App',
+}
+</script>
+
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+  <div class="layout" id="app">
+    <the-navbar />
+    <main class="layout--content container">
+      <router-view />
+    </main>
+    <the-footer class="layout--footer" />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
+<style lang="scss" scoped>
+.layout {
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
 }
 </style>
