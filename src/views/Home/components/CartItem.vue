e55aa4a92ff4c51a970522caf8d46e5f8ec0c0d0
Iago <yagomello16@gmail.com>
2020-05-18 17:05:08 -0300

    feat(views): implement CartItem component in Home View
    Added CartItem local component to Home View

---
+++ src/views/Home/components/CartItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="cart-item">
+    <b-img class="cart-item--img" :src="imgSrc" :alt="item.alt" />
+    <span class="cart-item--title mx-2">{{ item.title }}</span>
+    <span class="cart-item--amount">x {{ item.amount }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CartItem',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    imgSrc() {
+      return require(`@/assets/img/${this.item.img}.jpg`)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.cart-item {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 18px;
+
+  &--img {
+    max-height: 50px;
+    max-width: 50px;
+  }
+
+  &--amount {
+    font-weight: bold;
+  }
+}
+</style>
