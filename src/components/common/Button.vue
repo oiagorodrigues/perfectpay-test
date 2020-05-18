fb1f0316fb8fd08cfe15e86b21197d0d804103c4
Iago <yagomello16@gmail.com>
2020-05-18 16:59:17 -0300

    feat(components): add Button common component

---
+++ src/components/common/Button.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  name: 'TheButton',
+  props: {
+    variant: {
+      type: String,
+      default: 'success',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+  },
+}
+</script>
+
+<template>
+  <b-btn
+    v-on="$listeners"
+    class="the-button"
+    :variant="variant"
+    :disabled="disabled"
+    :aria-label="name"
+  >
+    <slot />
+  </b-btn>
+</template>
+
+<style lang="scss" scoped>
+.the-button {
+  font-weight: bold;
+  transition: all 0.3s;
+
+  &.disabled,
+  &:disabled {
+    color: $secondary;
+    background-color: $light;
+    border-color: $light;
+    opacity: 1;
+  }
+}
+</style>
